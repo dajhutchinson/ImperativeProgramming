ee109f7ecc0f2f46726c2b809cfae1e1608e0796
Dom Hutchinson <domahutch@gmail.com>
2017-12-11 19:55:29 +0000

    Week12-Just downloaded files

---
+++ Week12/sketch.c
@@ -0,0 +1,5 @@
+/*
+ *  Reads in a custom binary file <.sketch>
+ *  These files contain graphic instructions (Recording a sketch & timings)
+ *  This program then translates these instructions into calls of a graphics module
+ */
